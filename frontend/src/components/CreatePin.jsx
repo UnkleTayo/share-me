bdae39cf332c232611ca18ba0256ddbe9e27bb8e
Adetayo Akinsanya <akinsanyaadetayo@gmail.com>
2022-01-11 11:53:36 +0100

    Pin component created

---
+++ frontend/src/components/CreatePin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const createPin = () => {
+  return <div>i</div>
+}
+
+export default createPin
---
+++ frontend/src/components/MasonryLayout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MasonryLayout = () => {
+  return <div>Mansory</div>
+}
+
+export default MasonryLayout
